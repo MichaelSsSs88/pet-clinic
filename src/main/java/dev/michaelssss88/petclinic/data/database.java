4a5569ba0932914b92391638558b0ca0bb25cd89
MichaelSsSs88 <63695255+MichaelSsSs88@users.noreply.github.com>
2022-11-29 23:06:05 -0600

    Learning Spring boot

---
+++ src/main/java/dev/michaelssss88/petclinic/data/database.java
@@ -0,0 +1,5 @@
+package dev.michaelssss88.petclinic.data;
+
+public class database {
+
+}
---
+++ src/test/java/dev/michaelssss88/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.michaelssss88.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
