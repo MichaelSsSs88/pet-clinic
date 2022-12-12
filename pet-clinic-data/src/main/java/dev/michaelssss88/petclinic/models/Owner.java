cbbb3af0ca3801ab7ac138bae1c440031352acdf
MichaelSsSs88 <63695255+MichaelSsSs88@users.noreply.github.com>
2022-12-11 21:09:23 -0600

    Implement Map Based Services. closes #6

--- pet-clinic-data/src/main/java/dev/michaelssss88/petclinic/models/Owner.java
+++ pet-clinic-data/src/main/java/dev/michaelssss88/petclinic/models/Owner.java
@@ -1,4 +1,13 @@
 package dev.michaelssss88.petclinic.models;
 
 public class Owner {
+    private Long id;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- pet-clinic-data/src/main/java/dev/michaelssss88/petclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/dev/michaelssss88/petclinic/models/Vet.java
@@ -1,4 +1,14 @@
 package dev.michaelssss88.petclinic.models;
 
 public class Vet extends Person{
+    private Long id;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
