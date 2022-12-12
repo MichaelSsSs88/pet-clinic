d114d2c3b1dd85a88cd1f02a691beb7d04d0bd5e
MichaelSsSs88 <63695255+MichaelSsSs88@users.noreply.github.com>
2022-12-11 22:18:19 -0600

    Add Pet Clinic Index and controllers. closes #8

---
+++ pet-clinic-web/src/main/java/dev/michaelssss88/petclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package dev.michaelssss88.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"","/","index","index.html"})
+    public String index(Model model){
+        return "index";
+    }
+}
