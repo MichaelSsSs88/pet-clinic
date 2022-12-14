fb81269f5ec9790d39d82bc38c0f2025a02d3adb
MichaelSsSs88 <63695255+MichaelSsSs88@users.noreply.github.com>
2022-12-13 20:58:15 -0600

    List All Vets on index page. closes #13

--- pet-clinic-web/src/main/java/dev/michaelssss88/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/dev/michaelssss88/petclinic/controllers/VetController.java
@@ -1,12 +1,22 @@
 package dev.michaelssss88.petclinic.controllers;
 
+import dev.michaelssss88.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"/vets","/vets/index", "/vets/index.html"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
