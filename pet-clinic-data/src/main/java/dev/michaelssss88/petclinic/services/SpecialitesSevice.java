5cf793410e372b2e9cb3d0aba03bd9829fe17374
MichaelSsSs88 <63695255+MichaelSsSs88@users.noreply.github.com>
2022-12-20 23:48:18 -0600

    Create Speciality Service #28 . closes #28

---
+++ pet-clinic-data/src/main/java/dev/michaelssss88/petclinic/services/SpecialitesSevice.java
@@ -0,0 +1,6 @@
+package dev.michaelssss88.petclinic.services;
+
+import dev.michaelssss88.petclinic.models.Speciality;
+
+public interface SpecialitesSevice extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/dev/michaelssss88/petclinic/services/map/SpecialitesServiceMap.java
@@ -0,0 +1,36 @@
+package dev.michaelssss88.petclinic.services.map;
+
+import dev.michaelssss88.petclinic.models.Speciality;
+import dev.michaelssss88.petclinic.services.SpecialitesSevice;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialitesServiceMap extends AbstractMapService<Speciality, Long>  implements SpecialitesSevice {
+    @Override
+    public Set<Speciality> findAll() {
+        return  super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+
+    }
+}
